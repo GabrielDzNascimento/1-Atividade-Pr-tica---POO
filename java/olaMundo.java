7a92e7127c02f4a8563940469bb7dab1ca1c05db
Gabriel Diniz <gabriel7diniz@outlook.com.br>
2025-09-15 15:26:07 -0300

    Removendo arquivos desnecessários

--- java/olaMundo.java
+++
@@ -1,9 +0,0 @@
-// Código está contido na classe olaMundo
-// O nome do arquivo deve corresponder ao nome da Classe
-// Todas instruções executáveis tem que estar em um método
-// ; no final   
-public class olaMundo {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
